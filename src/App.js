c2127a63635cfd9f779759064ee6286603fe0393
“Anas <2436370s@student.gla.ac.uk>
2021-04-25 08:07:33 +0300

    Search box for desktop

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import './scss/App.scss';
-import patternbg from './images/pattern-bg.png';
+import patternbg from './components/images/pattern-bg.png';
 import Map from './components/Map';
+import Search from './components/Search';
 
 function App() {
   return (
     <div className="App">
-      <img src={patternbg}></img>
+      <h1>IP Address Tracker</h1>
+      <Search></Search>
+      <img className='background-img' src={patternbg} alt='backgroundimage'></img>
       <Map></Map>
     </div>
   );
